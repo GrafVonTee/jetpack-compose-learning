fbb91a7dd223943b12610d92a8eff567ebf78162
grafvontee <i@grafvontee.ru>
2024-03-30 18:17:19 +0300

    Material3 Canvas Shape Examples

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -45,7 +46,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,54 +63,93 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCourseTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column (
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        Button(onClick = { /*TODO*/ }) {
-                            Text(text = "Confirm")
-                        }
-
-                        ElevatedButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Outlined.Add,
-                                contentDescription = "Add to cart",
-                                modifier = Modifier.size(18.dp)
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(text = "Add to cart")
-                        }
-                        
-                        FilledTonalButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Open in browser")
-                        }
-
-                        OutlinedButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Back")
-                        }
-                        
-                        TextButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Learn more")
-                        }
-                        
-                        Spacer(modifier = Modifier.height(18.dp))
-
-                        Row {
-                            Button(onClick = { /*TODO*/ }) {
-                                Text(text = "Create a new account")
-                            }
-                            OutlinedButton(onClick = { /*TODO*/ }) {
-                                Text(text = "I have an existing account")
-                            }
-                        }
-                    }
-                }
+                MyCanvas()
             }
         }
+    }
+}
+
+@Composable
+fun MyCanvas() {
+    Canvas(
+        modifier = Modifier
+            .padding(20.dp)
+            .size(300.dp),
+    ) {
+        drawRect(
+            color = Color.Black,
+            size = size
+        )
+        drawRect(
+            color = Color.Red,
+            topLeft = Offset(100f, 100f),
+            size = Size(100f, 100f),
+            style = Stroke(
+                width = 10f,
+            )
+        )
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Yellow, Color.Red, Color.Black),
+                center = center - Offset(75f, 75f),
+                radius = 250f
+            ),
+            radius = 100f,
+            center = center
+        )
+
+        drawArc(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Yellow, Color.Red, Color.Black),
+                center = Offset(100f, 500f) + Offset(200f / 2, 200f / 2),
+                radius = 200f
+            ),
+            startAngle = 0f,
+            sweepAngle = 270f,
+            useCenter = true,
+            topLeft = Offset(100f, 500f),
+            size = Size(200f, 200f),
+        )
+        drawArc(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Cyan, Color.Blue, Color.Black),
+                center = Offset(100f, 500f) + Offset(200f / 2, 200f / 2),
+                radius = 200f
+            ),
+            startAngle = 270f,
+            sweepAngle = 90f,
+            useCenter = true,
+            topLeft = Offset(100f, 500f),
+            size = Size(200f, 200f),
+        )
+
+        drawArc(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Cyan, Color.Blue, Color.Black),
+                center = Offset(500f, 500f) + Offset(200f / 4, 200f / 4),
+                radius = 200f
+            ),
+            startAngle = 0f,
+            sweepAngle = 270f,
+            useCenter = false,
+            topLeft = Offset(500f, 500f),
+            size = Size(200f, 200f),
+            style = Stroke(
+                width = 5.dp.toPx(),
+            )
+        )
+
+        drawOval(
+            color = Color.Magenta,
+            topLeft = Offset(500f, 100f),
+            size = Size(200f, 300f),
+        )
+
+        drawLine(
+            color = Color.Cyan,
+            start = Offset(300f, 700f),
+            end = Offset(700f, 700f),
+            strokeWidth = 5.dp.toPx(),
+        )
     }
 }
