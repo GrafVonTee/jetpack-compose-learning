3103782a5c0f09b067d5f6b3d6df290fdd0162ee
grafvontee <i@grafvontee.ru>
2024-03-31 13:20:37 +0300

    Added Material3 dropdown menu

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -13,10 +13,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -68,6 +77,8 @@ class MainActivity : ComponentActivity() {
                             maxNumber = 8888,
                             text = "Статистика за всё время",
                         )
+
+                        DropDownMenu(listOf("За день", "За неделю", "За месяц", "За всё время"))
                     }
                 }
 
@@ -75,6 +86,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
 
 @Composable
 fun StatisticsByPeriod(
@@ -90,6 +103,55 @@ fun StatisticsByPeriod(
         fontSize = headerFontSize,
     )
     LinearProgressBar(percentage, maxNumber, width = 200.dp)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropDownMenu(
+    optionList: List<String> = listOf("None")
+) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    var selectedOption by remember {
+        mutableStateOf(optionList[0])
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = !isExpanded },
+        ) {
+            TextField(
+                modifier = Modifier.menuAnchor(),
+                value = selectedOption,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded) },
+            )
+
+            ExposedDropdownMenu(
+                expanded = isExpanded,
+                onDismissRequest = { isExpanded = false }
+            ) {
+                optionList.forEachIndexed { index, text ->
+                    DropdownMenuItem(
+                        text = { Text(text = text) },
+                        onClick = {
+                            selectedOption = optionList[index]
+                            isExpanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
